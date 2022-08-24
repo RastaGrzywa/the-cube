7964234539b1f64ce3acb71c4fb66e33c187dfd4
Andrzej Fejklowicz <andrzej@fejklowicz.com>
2022-08-24 19:08:47 +0200

    React client

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -6,6 +7,8 @@ dotenv.config();
 import User from './model/User.js';
 
 const app = express()
+
+const __dirname = path.resolve();
 
 async function main () {
     await mongoose.connect(process.env.DB_URL);
@@ -16,22 +19,32 @@ async function main () {
         console.log('Mongoose default connection open');
     }); 
 
-    User.find({}, function(err, users) {
-        console.log(users);
-    });
+    // User.find({}, function(err, users) {
+    //     console.log(users);
+    // });
 
-    // A sample route
-    app.get('/', (req, res) => {
 
-        res.send('Hello World!')
-    })
+
+    app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+    // // A sample route
+    // app.get('/', (req, res) => {
+
+    //     res.send('Hello World!')
+    // })
 
     app.get("/api", (req, res) => {
         res.json({ message: "API from server!" });
     });
 
+
+    // All other GET requests not handled before will return our React app
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    });
+    
     // start the server listening for requests
-    app.listen(process.env.PORT || 3000, 
+    app.listen(process.env.PORT || 3001, 
         () => console.log("Server is running..."));
 
 }
--- model/User.js
+++ model/User.js
@@ -1,14 +1,11 @@
 import mongoose from 'mongoose';
-import Human from './Human';
-import Player from './Player';
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
   platform:  String,
   platform_id: String,
   registered: { type: Date, default: Date.now },
-  player: Player,
-  human: Human
+
 });
 
 
